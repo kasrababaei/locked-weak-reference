9fe78f2a159aa726444238628b35207c82033f97
Kasra Babaei <kasrababaei@gmail.com>
2025-08-25 22:31:21 -0400

    Remove the client from release

--- Package.swift
+++ Package.swift
@@ -1,43 +1,47 @@
 // swift-tools-version: 6.1
 
+import Foundation
 import PackageDescription
 import CompilerPluginSupport
 
-let package = Package(
+var package = Package(
     name: "locked-weak-reference",
     platforms: [.macOS(.v10_15), .iOS(.v13)],
     products: [
-        .library(
-            name: "LockedWeakReference",
-            targets: ["LockedWeakReference"]
-        ),
-        .executable(
-            name: "LockedWeakReferenceClient",
-            targets: ["LockedWeakReferenceClient"]
-        ),
+        .library(name: "LockedWeakReference", targets: ["LockedWeakReference"]),
     ],
     dependencies: [
         .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "601.0.0-latest"),
     ],
     targets: [
-        .macro(
+        Target.macro(
             name: "LockedWeakReferenceMacro",
             dependencies: [
                 .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
                 .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
             ]
         ),
-
-        .target(name: "LockedWeakReference", dependencies: ["LockedWeakReferenceMacro"]),
-
-        .executableTarget(name: "LockedWeakReferenceClient", dependencies: ["LockedWeakReference"]),
-
-        .testTarget(
-            name: "LockedWeakReferenceTests",
-            dependencies: [
-                "LockedWeakReferenceMacro",
-                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
-            ]
-        ),
+        Target.target(name: "LockedWeakReference", dependencies: ["LockedWeakReferenceMacro"]),
     ]
 )
+
+// Toggle this flag to include the client and test target for develop builds.
+let isDevelop = false
+if isDevelop {
+    package.products.append(
+        .executable(name: "LockedWeakReferenceClient", targets: ["LockedWeakReferenceClient"])
+    )
+    
+    package.targets.append(
+        contentsOf: [
+            .executableTarget(name: "LockedWeakReferenceClient", dependencies: ["LockedWeakReference"]),
+            .testTarget(
+                name: "LockedWeakReferenceTests",
+                dependencies: [
+                    "LockedWeakReferenceMacro",
+                    .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+                ]
+            )
+        ]
+    )
+}
